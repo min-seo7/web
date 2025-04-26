1f268f264cb4115a191c54ae4226b0540e151456
min-seo7 <mms1117777@gmail.com>
2025-04-26 15:34:56 +0900

    과제수정

--- js/task/task2_0425.js
+++ js/task/task2_0425.js
@@ -1,13 +1,10 @@
 //두 수를 랜덤하게 출력하여 대소비교.[수 범위: 40 ~ 70]
+
+// 2. 임의의(Math.random) 두 개의 수(범위: 40 ~ 70)를 생성. "2개의 수중에서 큰 값은 ??, 작은값은 ??"
+
 let ReNum1 = 40 + Math.floor(Math.random()*31); 
 let ReNum2 = 40 + Math.floor(Math.random()*31); 
 
-console.log(ReNum1, ReNum2)
+console.log("랜덤하게 나온 두 수는 "+ ReNum1+" 와와 " + ReNum2 + " 입니다.");
 
-if(ReNum1 > ReNum2) {
-    console.log("두 개의 수중에서 큰 값은 " + ReNum1 + " 이고 작은 값은 " + ReNum2 + " 입니다." );
-} else if(ReNum2 > ReNum1) {
-    console.log("두 개의 수중에서 큰 값은 " + ReNum2 + " 이고 작은 값은 " + ReNum1 + " 입니다." );
-} else {
-    console.log("두 수의 값은 동일합니다.");
-}
+console.log("두 개의 수 중에서 큰 값은 " + Math.max(ReNum1, ReNum2) +" 이고 작은 값은 " + Math.min(ReNum1, ReNum2) + " 입니다.")
